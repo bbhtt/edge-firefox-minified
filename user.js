a549f4f5ddde315b8359fde20512938dac9816d4
bbhtt <bbhtt.zn0i8@slmail.me>
2023-12-10 13:11:55 +0530

    Add user.js

---
+++ user.js
@@ -0,0 +1,4 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("svg.context-properties.content.enabled", true);
+user_pref("layout.css.color-mix.enabled", true);
+user_pref("layout.css.light-dark.enabled", true);
